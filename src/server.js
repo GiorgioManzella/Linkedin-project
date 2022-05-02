e938329f35ea0438ef55e736cadeb817f7af5ef9
giorgio <giorgiolink94@hotmail.it>
2022-05-02 09:02:15 +0100

    Update layout

---
+++ src/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import listEndpoints from "express-list-endpoints";
+
+const server = express();
+const port = process.env.PORT || 3003;
+
+// middlewares ----------------------------------------------------------------
+
+server.use(cors());
+server.use(express.json());
+
+//endpoints ----------------------------------------------------------------
+
+// server.use("/product", productRouter);
+// server.use("/reviews", reviewRouter);
+
+// error handlers ----------------------------------------------------------------
+
+//connection to db-----------------------------------------------------------
+
+mongoose.connect(
+  "mongodb+srv://test:test@cluster0.teeo4.mongodb.net/Linkedin?retryWrites=true&w=majority"
+);
+mongoose.connection.on("connected", () => {
+  console.log("successfully connected"),
+    server.listen(port, () => {
+      console.table(listEndpoints(server));
+      console.log(`server is running on port: ${port}`);
+    });
+});
